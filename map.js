7c568e7a11e5da7ee435b4de135b56a2617cb604
KawserAlam21 <md.kawseralam983@gmail.com>
2021-10-06 21:18:34 +0600

    added map, filter and find

---
+++ map.js
@@ -0,0 +1,35 @@
+const numbers = [12, 3, 6, 11, 8, 1, 3, 2, 4, 9];
+// const squareNum = [];
+
+// for(let i = 0; i < numbers.length; i++){
+//     const number = numbers[i];
+//     const square = number * number;
+//     squareNum.push(square);
+// };
+// console.log(squareNum);
+
+// function double (element){
+//     return element * element;
+// }
+// const doubleIt = numbers.map(double);
+// console.log(doubleIt);
+
+// const makeDouble = numbers.map(doubleIt = element => element * element);
+// console.log(makeDouble);
+
+
+// const double = numbers.map(element => element * element);
+// console.log(double);
+
+// const doubleIt = numbers.map(x => x * x);
+// console.log(doubleIt);
+
+// const filterIt = numbers.filter(x => x <  5);
+// console.log(filterIt);
+
+// const result = numbers.find(element => element > 3);
+// console.log(result);
+
+const result = numbers.find(x => x < 5);
+console.log(result);
+
---
+++ true-false.js
@@ -0,0 +1,5 @@
+//Falsy Value
+// 0, NaN, Undefine, false, "", null
+
+//Truthy Value
+//'0', " ", {}, [];
